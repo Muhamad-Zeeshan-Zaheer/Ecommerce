2c2f24c0e7fee6786803386a5cc02820197a05e3
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2026-04-26 06:37:33 +0500

    completed the sti

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  before_validation :set_default_role, on: :create
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def set_default_role
+    self.role ||= "Customer"
+  end
 end
